d593886e49194150ddb5132fd8522096b26d990d
Ylimegirl <87622158+Ylimegirl@users.noreply.github.com>
2021-07-23 20:32:13 -0700

    JavaScript Maintenance
    Streamlined and updated JavaScript functions
    Added relevant CSS classes for JavaScript functions

--- assets/js/booklet.js
+++ assets/js/booklet.js
@@ -1,29 +1,53 @@
 $(document).ready(function(){
 	"use strict";//no clue what this means tbh but i think its important
-	var page = "01";// set the "page" the booklet is initially open to
-	var finalID = document.getElementById("album").lastElementChild.id;//grab the final "page" of the booklet by the ID of the last element nested under the element with the "album" ID
-	var max = finalID.substring(1,3)/2+1;//grabs the max page number by taking the number portion of finalID (i.e. #p00) and then converts that into the amount of spreads the booklet has by dividing it in half and adding one (to account for the "spreads" for the front and back covers as well)
-	if(max < 10)//if the max value is less than 10
-		max="0"+max;//it adds a leading zero to the max number
+	var spread = "01";// set the spread the booklet is initially open to
+	var finalID = document.getElementById("album").lastElementChild.id;//grab the final page number of the booklet by the ID of the last element nested under the element with the "album" ID
+	var max = leadingZero(finalID.substring(1,3)/2+1);//grabs the max spread number by taking the number portion of finalID (i.e. #p00) and then converts that into the amount of spreads the booklet has by dividing it in half and adding one (to account for the "spreads" of the front and back covers as well), and then adds a leading zero if needed
 	//console.log("Ready"); //for debugging
-	$("figure").css("visibility", "hidden");//sets all pages to No Appear by default
-	$(".g01").css("visibility", "visible");//displays the elements in the first spread of the booklet
+	
+	$("figure").removeClass("current").addClass("hidden");//sets all pages to No Appear by default
+	showSpread(spread);//displays the elements in the first spread of the booklet
+	toggleJacket();//and also the jacket
+	
 	$("#buttonLeft").click(function() {//if the left arrow is clicked
-		if(page !== "01") {//and we're not on the first spread
-			$(".g"+page).css("visibility", "hidden");//disappear the current spread
-			page--;//move the spread down by one
-			if(page<10)//if the current spread value is less than 10
-			page="0"+page;//add a leading zero
-			$(".g"+page).css("visibility", "visible");//and then display the corresponding spread
+		if(spread !== "01") {//and we're not on the first spread
+			hideSpread(spread);//disappear the current spread
+			spread--;//move the spread down by one
+			spread = leadingZero(spread);//add a leading zero, if needed
+			showSpread(spread)//and then display the corresponding spread
+			toggleJacket();//toggle the visibility of the jacket
 		}
 	});
+	
 	$("#buttonRight").click(function() {//if the right arrow is clicked
-		if(page !== max) {//and we're not on the last spread
-			$(".g"+page).css("visibility", "hidden");//disappear the current spread
-			page++;//move the spread up by one
-			if(page<10)//if the current spread value is less than 10
-			page="0"+page;//add a leading zero
-			$(".g"+page).css("visibility", "visible");//and then display the corresponding spread
+		if(spread !== max) {//and we're not on the last spread
+			hideSpread(spread);//disappear the current spread
+			spread++;//move the spread up by one
+			spread = leadingZero(spread);//add a leading zero, if needed
+			showSpread(spread)//and then display the corresponding spread
+			toggleJacket();//toggles the visibility of the jacket
 		}
 	});
-});//oh god i added these comments like two years after i first wrote the code and had to try and figure out my prior self's thought process. fun times
+	
+	function toggleJacket() {//toggles the visibility of the jacket
+		if(spread == "01")
+			$("#jacket").removeClass("hidden").addClass("current");
+		else
+			$("#jacket").removeClass("current").addClass("hidden");
+	};
+	
+	function showSpread(a) {//shows an inputted spread
+		$(".g"+a).removeClass("hidden").addClass("current");
+	};
+	
+	function hideSpread(a) {//hides an inputted spread
+		$(".g"+a).removeClass("current").addClass("hidden");
+	};
+	
+	function leadingZero(a) {//adds a leading zero if a number is only one digit
+		if(a < 10)
+			return "0"+a;
+		else
+			return a;
+	};
+});//oh god i first added these comments like two years after i first wrote the code and had to try and figure out my prior self's thought process. fun times
